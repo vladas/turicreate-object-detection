24bf9f0af7bffb701984bc0932d68ef41aa22bb6
Vladas Diržys <v.dirzys@sportradar.com>
2019-05-15 23:47:07 +0200

    quite download

--- download_training_data.sh
+++ download_training_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget -O /storage/xy_signs.sframe.tar.gz https://dl.dropboxusercontent.com/s/cc5ifcxz34mdqtf/xy_signs.sframe.tar.gz?dl=0
+wget -q -O /storage/xy_signs.sframe.tar.gz https://dl.dropboxusercontent.com/s/cc5ifcxz34mdqtf/xy_signs.sframe.tar.gz?dl=0
 
 cd /storage/
 
