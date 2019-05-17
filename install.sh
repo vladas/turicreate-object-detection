1183017f61aa749707c19eb5d0484095a731162c
Vladas Diržys <v.dirzys@sportradar.com>
2019-05-17 22:12:46 +0200

    Install mxnet

--- install.sh
+++ install.sh
@@ -5,4 +5,4 @@ pip list
 ls -la /usr/local/lib/python2.7/dist-packages/turicreate/toolkits/
 
 
-pip uninstall -y mxnet && pip install mxnet-cu90==1.1.0
+pip uninstall -y mxnet && pip install mxnet==1.1.0
